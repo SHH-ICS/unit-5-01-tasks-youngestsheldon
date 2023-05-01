25f9b838ec84cdd3aed518aed8ded392e4c48760
youngestsheldon <haaris.Khokhar@stu.ocsb.ca>
2023-05-01 14:17:44 +0000

    googer

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,8 +1,8 @@
-import.math
+import math
 
 y = input("Input value a ")
 x = input("Input value b ")
 
-vtuber feet = math.sqrt((x**2) + (y**2))
+vtuberfeet = math.sqrt((x**2) + (y**2))
 
-print(vtuber feet)
+print(vtuberfeet)
